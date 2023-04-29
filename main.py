b338ef63acc23594efe069d0fcb687f23c79d808
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-04-29 01:24:24 +0100

    Revert "Rename app variable"
    This reverts commit 24b0eab2fe317c3fc35c669a64c31bba3397b98b.

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 # Configure Flask app
 def create_app():
-    application = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True)
     app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///war_cor.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    return application
+    return app
 
 
 app = create_app()
