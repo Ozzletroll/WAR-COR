40ce25f4dbb7c7621f88428272444688360df768
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-04-29 01:22:23 +0100

    Refactor flask app configuration

--- main.py
+++ main.py
@@ -12,13 +12,20 @@ from flask_sqlalchemy import SQLAlchemy
 # TODO: Define Event database models
 # TODO: Define Comment database models
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+
+# Configure Flask app
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+    app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///war_cor.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    return app
+
+
+app = create_app()
 
 
 # Configure database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///war_cor.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
