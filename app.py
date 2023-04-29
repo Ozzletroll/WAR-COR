af3e9166a448777623ba5c04bba1de13fe30bcfd
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-04-29 02:50:10 +0100

    Add comments

--- app.py
+++ app.py
@@ -5,17 +5,18 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-# Configure Flask app
+# Application factory
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///war_cor.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # Initialise database
+    # Initialise database and import models
     db.init_app(app)
     import models
 
+    # Create the database
     with app.app_context():
         db.create_all()
 
