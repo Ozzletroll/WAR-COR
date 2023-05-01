9b05bfeea5d50b152a74cd6f98dfad692250287a
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-05-02 00:59:27 +0100

    Instantiate login manager

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 
@@ -19,6 +20,10 @@ def create_app():
     # Create the database
     with app.app_context():
         db.create_all()
+
+    # Login manager
+    login_manager = LoginManager()
+    login_manager.init_app(app)
 
     return app
 
