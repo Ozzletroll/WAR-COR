61573bde6804d8868a3a39e339619d43fc89e297
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-05-16 21:39:46 +0100

    Refactor to allow alternate databse uri for tests

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ db = SQLAlchemy()
 
 
 # Application factory
-def create_app():
+def create_app(database_uri='sqlite:///war_cor.db'):
     app = Flask(__name__, instance_relative_config=True)
     app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///war_cor.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # Initialise database and import models
