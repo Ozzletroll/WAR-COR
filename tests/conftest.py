36c5960c59538a1fe5210e9a0dd7afd7714a67df
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-05-18 01:39:24 +0100

    Update syntax for consistency

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,8 +12,8 @@ def app():
 
 @pytest.fixture()
 def client(app):
-    app.config.update({"TESTING": True})
     # app.config['WTF_CSRF_ENABLED'] = False
+    app.config["TESTING"] = True
     app.config['SECRET_KEY'] = "TESTING_KEY"
     app.config['WTF_CSRF_ENABLED'] = False
     with app.test_client() as client:
