298b564a1e48cf0b969a88836c543c53dc7d608b
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-05-22 15:47:57 +0100

    Remove TODO tasks

--- main.py
+++ main.py
@@ -1,31 +1,6 @@
 from app import create_app
 from routes import configure_routes
 
-# Initial setup:
-# TODO: Create and style page templates
-#     TODO: Create index.html
-#     TODO: Create register.html
-#     TODO: Create login.html
-#     TODO: Create user_settings.html
-#     TODO: Create timeline.html
-#     TODO: Create new_campaign.html
-#     TODO: Create edit_campaign.html
-#     TODO: Create event.html
-#     TODO: Create new_event.html
-#     TODO: Create edit_event.html
-
-# TODO: Implement basic page navigation
-
-# TODO: Main functionality
-#   TODO: Add campaign editing
-#   TODO: Add campaign deletion
-#   TODO: Add campaign user invitation
-
-#   TODO: Add event viewing
-#   TODO: Add event editing
-#   TODO: Add event deletion
-#   TODO: Add event commenting
-
 flask_app = create_app()
 configure_routes(flask_app)
 
