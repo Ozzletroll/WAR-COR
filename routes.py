20ed4eea6abb4a75f792c9e1f1220f18b4ac695d
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-05-22 19:20:11 +0100

    Fix redirect upon succesful campaign creation
    Adds missing campaign_title to dynamic url

--- routes.py
+++ routes.py
@@ -241,7 +241,9 @@ def configure_routes(flask_app):
                     db.session.add(event)
                     db.session.commit()
 
-                    return redirect(url_for("show_timeline", id=target_id))
+                    return redirect(url_for("show_timeline",
+                                            campaign_name=campaign_query.title,
+                                            id=target_id))
 
                 return render_template("new_event.html", form=form)
 
