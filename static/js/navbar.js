9c8fade07ada30f0cc6c51a33a23d3b7fc690772
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-06-05 18:49:26 +0100

    Hide hamburger menu if screen resized

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,8 +1,27 @@
+// Continously check screen size
+function handleResize() {
+  let newHeight = window.innerHeight;
+  let newWidth = window.innerWidth;
+  var menu = document.getElementById("hamburgerMenu");
+
+  // Hide hamburger menu if screen has been resized beyond 700px
+  if (newWidth >= 700) {
+    menu.style.display = "none"
+  }
+
+}
+
+window.addEventListener("resize", handleResize);
+
+// calling the resize function for the first time
+handleResize();
+
+// Display the hamburger menu on click
 function hamburgerClick() {
-  var x = document.getElementById("hamburgerMenu");
-  if (x.style.display === "flex") {
-    x.style.display = "none";
+  var menu = document.getElementById("hamburgerMenu");
+  if (menu.style.display === "flex") {
+    menu.style.display = "none";
   } else {
-    x.style.display = "flex";
+    menu.style.display = "flex";
   }
 }
