856dcea1f98fcb2ca69ee709938d27e938a25ebe
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-06-09 01:24:48 +0100

    Add modal1 styling

---
+++ static/js/index.js
@@ -0,0 +1,17 @@
+var modal = document.getElementById("Modal1");
+var btn = document.getElementById("m1_button");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "flex";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
