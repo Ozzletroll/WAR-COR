8de151c82791cb1913c0cdd96f431c50b1c84567
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-07-21 16:25:38 +0100

    Add timeline margin adjustment when using sidebar

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -4,24 +4,30 @@ class Tab {
     tab,
     button,
     icon,
+    timeline
   }) {
     this.tab = document.getElementById(tab);
     this.button = button;
     this.button_elem = document.getElementById(button);
     this.state = false
     this.icon = document.getElementById(icon);
+    this.timeline = document.getElementById(timeline);
+
+    this.updateTimelineMargin();
+
+    window.addEventListener('resize', this.updateTimelineMargin.bind(this));
 
     document.getElementById(this.button).onclick = event => {
-      
+
       if (this.state == false) {
         this.openTab(event)
       }
       else if (this.state == true) {
         this.closeTab(event)
       }
-    } 
+    }
   }
-  
+
   openTab() {
     this.tab.style.transform = "translateX(0)"
     this.button_elem.style.transform = "translateX(0)"
@@ -36,14 +42,24 @@ class Tab {
     this.tab.style.marginRight = "-300px"
     this.state = false
     this.icon.src = "/static/images/icons/chevron_right.svg"
+    this.timeline.style.marginLeft = "0"
   }
 
+  // Adjust margin if sidebar deployed and screen between 800 and 850px
+  updateTimelineMargin() {
+    if (window.innerWidth >= 800 && window.innerWidth <= 900 && this.state == true) {
+      this.timeline.style.marginLeft = "-15%";
+    } else {
+      this.timeline.style.marginLeft = "";
+    }
+  }
 }
 
 
 // Create sidebar
-const tab_1 = new Tab({
+const sidebar = new Tab({
   tab: "sidebar",
   button: "sidebar-deploy",
-  icon: "sidebar-icon"
+  icon: "sidebar-icon",
+  timeline: "timeline-container"
 })
