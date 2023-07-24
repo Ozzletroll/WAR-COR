1c773cf761fba7f7c6b5d645e629d5f07d6fe94c
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-07-24 16:34:22 +0100

    Add on click animation for timeline events

---
+++ static/js/timeline.js
@@ -0,0 +1,57 @@
+// Dropdown class
+class Event {
+  constructor({
+    dropdown,
+    button,
+  }) {
+    this.dropdown = dropdown;
+    this.button = button;
+    this.state = false
+
+    this.button.onclick = event => {
+      
+      if (this.state == false) {
+        this.openDropdown(event)
+      }
+      else if (this.state == true) {
+        this.closeDropdown(event)
+      }
+    } 
+
+  }
+  
+  openDropdown() {
+    var contentHeight = this.dropdown.scrollHeight;
+    this.dropdown.style.minHeight = contentHeight + "px";
+    this.state = true
+  }
+
+  closeDropdown() {
+    this.dropdown.style.minHeight = "0"
+    this.state = false
+  }
+
+}
+
+// Create array to hold dropdown objects
+const eventItems = []
+
+// Select all dropdown elements
+var buttons = document.querySelectorAll('[id^="event-"]');
+var dropdowns = document.querySelectorAll('[id^="dropdown-"]');
+
+// Iterate through both arrays, creating dropdown objects
+buttons.forEach((button, index) => {
+
+  var event = new Event({
+    dropdown: dropdowns[index],
+    button: button,
+  })
+
+  // Append object to array
+  eventItems.push(event)
+});
+
+
+
+
