3f3db7f6996b1851af1b6c90060c033294cee9bb
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-08-03 12:27:23 +0100

    Add flask-migrate migration support

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 from utils.generators import create_data
 
@@ -24,6 +25,9 @@ def create_app(database_uri='sqlite:///war_cor.db', test_config=None):
     # Create the database
     with app.app_context():
         db.create_all()
+
+    # Initialise db migrations
+    migrate = Migrate(app, db)
 
     # Initialise login manager
     login_manager = LoginManager()
