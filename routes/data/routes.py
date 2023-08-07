b72b973b6444a4472bb30a2ad3ed9132f2ca29e9
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-08-07 14:46:19 +0100

    Add placeholder data backup page

--- routes/data/routes.py
+++ routes/data/routes.py
@@ -1,12 +1,12 @@
 from flask import render_template, redirect, request, url_for, flash, jsonify, make_response
 from sqlalchemy import select
-from flask_login import login_required, current_user
+from flask_login import login_required
 
 import auth
-import forms
 import models
 import utils.organisers as organisers
 import utils.messengers as messengers
+import utils.serialisers as serialisers
 
 from routes.data import bp
 from app import db
@@ -18,32 +18,42 @@ from app import db
 #   =======================================
 
 
-# Backup campaign data
-@bp.route("/campaigns/<campaign_name>/backup")
-def campaign_backup(campaign_name):
+# Data backup main page
+@bp.route("/campaigns/<campaign_name>/data")
+@login_required
+def backup_page(campaign_name):
 
     target_campaign_id = request.args["campaign_id"]
-
     campaign = db.session.execute(select(models.Campaign).filter_by(id=target_campaign_id, title=campaign_name)).scalar()
 
+    # Check if the user has permissions to edit the target campaign.
+    auth.permission_required(campaign)
+
+    return render_template("backup.html", campaign=campaign)
+
+
+# Backup campaign data
+@bp.route("/campaigns/<campaign_name>/data/export")
+@login_required
+def campaign_backup(campaign_name):
+
+
+    target_campaign_id = request.args["campaign_id"]
+    campaign = db.session.execute(select(models.Campaign).filter_by(id=target_campaign_id, title=campaign_name)).scalar()
+
+    # Check if the user has permissions to edit the target campaign.
+    auth.permission_required(campaign)
+
     # Export campaign data as json file.
+    serialisers.data_export(campaign)
 
     return redirect(url_for("campaign.campaigns"))
 
 
 # Import campaign backup
-@bp.route("/campaigns/<campaign_name>/import")
+@bp.route("/campaigns/<campaign_name>/data/import")
+@login_required
 def import_campaign(campaign_name):
-
-    target_campaign_id = request.args["campaign_id"]
-
-    # Export campaign data as json file.
-    return redirect(url_for("campaign.show_timeline", id=target_campaign_id))
-
-
-# Export campaign timeline as pdf
-@bp.route("/campaigns/<campaign_name>/export")
-def export_campaign(campaign_name):
 
     target_campaign_id = request.args["campaign_id"]
 
