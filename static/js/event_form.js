66fa88636fe77fdb0ae6ff524135ef7c8a2c04df
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-08-21 19:39:35 +0100

    Add event form html preview

--- static/js/event_form.js
+++ static/js/event_form.js
@@ -29,6 +29,99 @@ class Modal {
 
 }
 
+
+
+// Preview Area Modal class
+class PreviewModal {
+  constructor({
+    modal,
+    button,
+    span,
+  }) {
+    this.modal = document.getElementById(modal);
+    this.button = button;
+    this.span = span;
+
+    document.getElementById(this.button).onclick = event => {
+      this.openModal(event)
+    } 
+
+    document.getElementById(this.span).onclick = event => {
+      this.closeModal(event)
+    } 
+
+  }
+  
+  openModal() {
+    this.modal.style.display = "flex";
+    this.htmlPreview()
+  }
+
+  closeModal() {
+    this.modal.style.display = "none";
+  }
+
+  htmlPreview() {
+
+    // Get the content of the CKEditor field
+    var editor = CKEDITOR.instances['form-contents'];
+    var content = editor.getData();
+  
+    // Get the preview modal text area and add html
+    var modalBody = document.getElementById("preview-modal-body");
+    modalBody.innerHTML = content;
+   
+    this.formatImages();
+  
+  }
+
+  formatImages() {
+    // Apply styling to user submitted image links
+    const elements = document.querySelectorAll(".modal-body-preview p");
+    let imageCount = 0;
+
+    elements.forEach((element) => {
+      if (element.querySelector("img")) {
+        imageCount++;
+
+    // Create surrounding element
+    const newDiv1 = document.createElement("div");
+    newDiv1.classList.add("user-image-area");
+
+    // Wrap the element within the new div
+    element.parentNode.insertBefore(newDiv1, element);
+    newDiv1.appendChild(element);
+
+    // Create surrounding element
+    const newDiv2 = document.createElement("div");
+    newDiv2.classList.add("user-image-elem");
+
+    // Wrap the element within the new div
+    element.parentNode.insertBefore(newDiv2, element);
+    newDiv2.appendChild(element);
+
+    // Create header
+    const newHeader = document.createElement("div");
+    newHeader.classList.add("user-image-header");
+    newHeader.textContent = `Image::Data_${String(imageCount).padStart(2, "0")}`;
+
+    // Insert the header
+    element.parentNode.insertBefore(newHeader, element);
+
+    // Centre img elements in user submitted p elements
+    element.style.display = "flex";
+    element.style.width = "auto";
+    element.style.justifyContent = "center";
+    element.style.margin = "0";
+
+    }});
+
+  }
+
+}
+
+
+
 // Create modals
 const modal_1 = new Modal({
   modal: "modal-1",
@@ -36,22 +129,22 @@ const modal_1 = new Modal({
   span: "close-1",
 })
 
-// Create modals
 const modal_2 = new Modal({
   modal: "modal-2",
   button: "event-belligerents-help-button",
   span: "close-2",
 })
 
-// Create modals
 const modal_3 = new Modal({
   modal: "modal-3",
   button: "event-format-help-button",
   span: "close-3",
 })
 
+// HTML Preview Area Modal
+const modal_preview = new PreviewModal({
+  modal: "modal-preview",
+  button: "html-preview-button",
+  span: "close-preview",
+})
 
-function htmlPreview() {
-  console.log("Preview")
-
-}
