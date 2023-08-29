cf44c520bf35cf6b35d9329799c2ebeee74120ea
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-08-29 17:41:08 +0100

    Add campaigns page layout toggle button

--- static/js/campaigns.js
+++ static/js/campaigns.js
@@ -59,4 +59,29 @@ buttons.forEach((button, index) => {
 });
 
 
+// Get the radio buttons
+const radioList = document.getElementById("campaign-toggle-list");
+const radioGrid = document.getElementById("campaign-toggle-grid");
 
+// Add event listener to each radio button
+radioList.addEventListener("click", toggleLabelBackground);
+radioGrid.addEventListener("click", toggleLabelBackground);
+
+// Add event listener for page load
+document.addEventListener("DOMContentLoaded", toggleLabelBackground);
+
+// Function to toggle label background
+function toggleLabelBackground() {
+  // Get the labels
+  const labelList = document.querySelector('label[for="campaign-toggle-list"]');
+  const labelGrid = document.querySelector('label[for="campaign-toggle-grid"]');
+
+  // Change label background based on checked status
+  if (radioList.checked) {
+    labelList.style.backgroundColor = "var(--elem_bright)";
+    labelGrid.style.backgroundColor = "";
+  } else if (radioGrid.checked) {
+    labelList.style.backgroundColor = "";
+    labelGrid.style.backgroundColor = "var(--elem_bright)";
+  }
+}
