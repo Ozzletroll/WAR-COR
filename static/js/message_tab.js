4156e1c87a1eebf1bde7140130f9cb1f1469d753
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-09-04 13:57:39 +0100

    Fix message tab script running when not logged in
    Splits navbar.js into navbar.js and message_tab.js.
    Adds jinja conditional to test if user is logged in
    before running.

---
+++ static/js/message_tab.js
@@ -0,0 +1,45 @@
+// Message tab class
+class messageTab {
+  constructor({
+    tab,
+    button,
+  }) {
+    this.tab = document.getElementById(tab);
+    this.button = button;
+    this.state = false
+
+    document.getElementById(this.button).onclick = event => {
+
+      if (this.state == false) {
+        this.openTab(event)
+      }
+      else if (this.state == true) {
+        this.closeTab(event)
+      }
+    }
+  }
+
+  openTab() {
+    this.tab.style.marginBottom = "-300px"
+    this.tab.style.height = "300px"
+    this.tab.style.borderBottom = "1px solid var(--elem_border)"
+    this.tab.style.borderTop = "1px solid var(--elem_border)"
+    this.state = true
+  }
+
+  closeTab() {
+    this.tab.style.marginBottom = "0"
+    this.tab.style.height = "0"
+    this.tab.style.borderBottom = "0px solid var(--elem_border)"
+    this.tab.style.borderTop = "0px solid var(--elem_border)"
+
+    this.state = false
+  }
+
+}
+
+// Create messages tab
+const messages_tab = new messageTab({
+  tab: "messages-tab",
+  button: "messages-button",
+})
