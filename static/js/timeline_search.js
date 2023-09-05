29691cf9ef034f1fed03acdcc6e7b62b3e201fd2
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-09-05 21:28:49 +0100

    Add new class based timeline search
    Returns results objects, allowing for easier page
    styling.

--- static/js/timeline_search.js
+++ static/js/timeline_search.js
@@ -1,58 +1,119 @@
-const searchBar = document.getElementById("search-bar");
-
 /**
-* Function to filter timeline page elements based on search bar query.
-* Hides elements that do not contain a matching query by setting opacity to 50%.
+* Search engine class
+* Takes the search bar input element in the constructor.
 */
-function timelineSearch() {
-  var searchQuery = searchBar.value.toLowerCase();
-  var monthOuters = document.getElementsByClassName("month-outer");
+class SearchEngine {
+  constructor(searchBar) {
+    this.searchBar = searchBar;
+    this.results = [];
+  }
 
-  // Iterate through all month-outer elements
-  for (var index = 0; index < monthOuters.length; index++) {
+  /**
+  * Timeline search method
+  * Takes the searchBar input and iterates through all event headers on the page.
+  * Returns Result objects and appends them to results array.
+  */
+  timelineSearch() {
 
-    var containsResult = false;
+    var searchQuery = this.searchBar.value.toLowerCase();
 
-    // Get the current month-outer element
-    var container = monthOuters[index];
+    // Check if old results are still valid
+    this.resultsCheck(searchQuery);
 
-    // Find all the elements with the class "event-header" within the container
-    var eventHeaders = container.querySelectorAll(".event-header");
+    var monthOuters = document.getElementsByClassName("month-outer");
+  
+    // Iterate through all month-outer elements
+    for (var outerIndex = 0; outerIndex < monthOuters.length; outerIndex++) {
 
-    // Iterate through all the event-header elements
-    for (var i = 0; i < eventHeaders.length; i++) {
-      var eventHeader = eventHeaders[i];
-      var elementText = eventHeader.innerText.toLowerCase();
+      // Get the current month-outer element
+      var container = monthOuters[outerIndex];
 
-      // Get event element and time label
-      var outerContainer = eventHeaders[i].closest('.event-outer-container');
-      var rightBranchLabel = outerContainer.previousElementSibling;
+      // Find all the elements with the class "event-header" within the container
+      var eventHeaders = container.querySelectorAll(".event-header");
+  
+      // Iterate through all the event-header elements
+      for (var headerIndex = 0; headerIndex < eventHeaders.length; headerIndex++) {
+        var eventHeader = eventHeaders[headerIndex];
+        var elementText = eventHeader.innerText.toLowerCase();
+  
+        // Compare searchQuery against event header text
+        if (elementText.includes(searchQuery)) {
 
-      // Positive search result
-      if (elementText.includes(searchQuery)) {
-        // Reset styling to undo any changes made by search function
-        outerContainer.style.opacity = "100%";
-        rightBranchLabel.style.opacity = "100%";
-        containsResult = true;
+          // Check if result is already in results array
+          var exists = this.results.some(result => result.elementText === elementText);
+          if (!exists) {
+
+            // If result doesn't already exist, create result object
+            var result = new Result({
+              elementText: elementText,
+              outerIndex: outerIndex,
+              headerIndex: headerIndex,
+              resultBelow: null,
+              scrollTarget: eventHeaders[headerIndex].closest('.timeline-event'),
+            })
+
+            // Append new result object to result list
+            this.results.push(result)
+
+          }
+
+        
+        }
+          
       }
-      // Negative search result
-      else {
-        outerContainer.style.opacity = "50%";
-        rightBranchLabel.style.opacity = "50%";
-      }
+  
     }
 
-    // If result not found in any sub elements, set opacity to 50%
-    if (containsResult == false && searchBar.value != "") {
-      monthOuters[index].style.opacity = "50%";
-    }
-    else {
-      monthOuters[index].style.opacity = "100%";
-    }
+    console.log("End of timeline search function:")
+    console.log(this.results)
+
 
   }
+
+  /**
+  * Method to iterate through all current results objects
+  * and remove ones that no longer match the search query.
+  */
+  resultsCheck(searchQuery) {
+
+    this.results.forEach((result, index) => {
+
+      // Remove result if it no longer matches the search query string
+      if (!result.elementText.includes(searchQuery)) {
+        this.results = this.results.splice(index, index)
+      }
+
+    });
+
 }
 
+}
+
+
+class Result {
+  constructor({
+    elementText, 
+    outerIndex, 
+    headerIndex, 
+    resultBelow, 
+    scrollTarget,
+  }) {
+    this.elementText = elementText;
+    this.outerIndex = outerIndex;
+    this.headerIndex = headerIndex;
+    this.resultBelow = resultBelow;
+    this.scrollTarget = scrollTarget;
+  }
+
+
+}
+
+
+
+// Get searchbar and create search engine
+const searchBar = document.getElementById("search-bar");
+const searchEngine = new SearchEngine(searchBar)
+
 // Add event listener to the input field
-searchBar.addEventListener("input", timelineSearch);
+searchBar.addEventListener("input", () => searchEngine.timelineSearch());
 
