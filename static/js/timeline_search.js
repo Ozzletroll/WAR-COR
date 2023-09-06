1532f505d2bda4b84b4311ce73c2739b69cff540
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-09-06 20:44:46 +0100

    Add timeline search greyout negative result lines
    Sets negative result elements to 50% opacity, but
    maintains timeline connecting line.
    NOTE: Timeline day marker elements are not yet greyed out
    properly!

--- static/js/timeline_search.js
+++ static/js/timeline_search.js
@@ -1,3 +1,130 @@
+class Result {
+  constructor({
+    positive,
+    elementText, 
+    outerIndex, 
+    headerIndex, 
+    resultsBelow, 
+    scrollTarget,
+    elements,
+  }) {
+    this.positive = positive;
+    this.elementText = elementText;
+    this.outerIndex = outerIndex;
+    this.headerIndex = headerIndex;
+    this.resultsBelow = resultsBelow;
+    this.scrollTarget = scrollTarget;
+    this.elements = elements;
+  }
+
+  /**
+    * Method to style result object
+    */
+  stylePositive() {
+
+    this.styleReset();
+
+    this.elements["headerElement"].style.opacity = "100%";
+
+    if (this.resultsBelow == false) {
+      this.elements["eventLine"].style.opacity = "50%";
+    }
+  }
+
+  /**
+  * Method to style negative result object
+  */
+  styleNegative() {
+
+    this.styleReset();
+
+    if (this.resultsBelow == false) {
+      this.elements["eventLine"].style.opacity = "50%";
+    }
+
+    this.elements["headerElement"].style.opacity = "50%";
+    this.elements["rightBranchLabel"].style.opacity = "50%";
+
+  }
+
+  styleReset() {
+    this.elements["headerElement"].style.opacity = "";
+    this.elements["eventLine"].style.opacity = "";
+    this.elements["rightBranchLabel"].style.opacity = "";
+  }
+
+}
+
+
+
+class Month {
+  constructor({
+    element,
+    events,
+  }) {
+    this.element = element;
+    this.events = events;
+  }
+
+  /**
+    * Method to determine if each event in month has elements below it
+    * in order to determine appropriate line styling.
+    */
+  checkResultsBelow() {
+    // Set the resultsBelow attribute to "true" if there are positive results below it
+    this.events.forEach((result, index) => {
+      result.resultsBelow = this.events.some((nextResult, nextIndex) =>
+        nextIndex > index && nextResult.positive
+      );
+    });
+
+  }
+
+
+  /**
+  * Method to style all search results within month
+  */
+  setStyle() {
+
+    // Reset month opacity
+    this.element.style.opacity = "";
+    this.resetAllStyles();
+
+    // Set month block opacity to 50% if it contains no positive results
+    const anyPositiveEvent = this.events.some(event => event.positive === true);
+    if (!anyPositiveEvent) {
+      this.element.style.opacity = "50%";
+    }
+
+    // If a block contains positive results, style each result accordingly
+    if (anyPositiveEvent) { 
+
+      // Reset any styling that may already be applied to result
+      this.events.forEach(result => {
+        result.styleReset();
+
+        if (result.positive) {
+          result.stylePositive();
+        }
+        else {
+          result.styleNegative();
+        }
+      });
+    }
+  }
+
+
+  /**
+  * Method to clear all styling from search result within the month
+  */
+  resetAllStyles() {
+    this.events.forEach(result => {
+      result.styleReset();
+    });
+  }
+
+}
+
 /**
 * Search engine class
 * Takes the search bar input element in the constructor.
@@ -6,6 +133,7 @@ class SearchEngine {
   constructor(searchBar) {
     this.searchBar = searchBar;
     this.results = [];
+    this.months = [];
   }
 
   /**
@@ -22,6 +150,13 @@ class SearchEngine {
 
     // Clear results and end operation if searchbar cleared
     if (searchQuery.length == 0){
+
+      // Reset styling for each month block
+      this.months.forEach(month => {
+        month.resetAllStyles();
+      });
+      // Clear all existing search attributes
+      this.months = [];
       this.results = [];
       return;
     }
@@ -34,6 +169,11 @@ class SearchEngine {
       // Get the current month-outer element
       var container = monthOuters[outerIndex];
 
+      var month = new Month({
+        element: container,
+        events: [],
+      })
+
       // Find all the elements with the class "event-header" within the container
       var eventHeaders = container.querySelectorAll(".event-header");
   
@@ -41,55 +181,63 @@ class SearchEngine {
       for (var headerIndex = 0; headerIndex < eventHeaders.length; headerIndex++) {
         var eventHeader = eventHeaders[headerIndex];
         var elementText = eventHeader.innerText.toLowerCase();
+
+        // Get result elements for styling
+        var outerContainer = eventHeaders[headerIndex].closest('.event-outer-container');
+        var rightBranchLabel = outerContainer.previousElementSibling;
+        var eventLine = rightBranchLabel.previousElementSibling;
   
+        // Create instance of result object
+        var result = new Result({
+          positive: false,
+          elementText: elementText,
+          outerIndex: outerIndex,
+          headerIndex: headerIndex,
+          resultsBelow: false,
+          scrollTarget: eventHeaders[headerIndex].closest('.timeline-event'),
+          elements: {
+            monthOuter: container,
+            headerElement: eventHeaders[headerIndex], 
+            rightBranchLabel: rightBranchLabel, 
+            eventLine: eventLine,
+          }
+        })
+
         // Compare searchQuery against event header text
         if (elementText.includes(searchQuery)) {
 
           // Check if result is already in results array
           var exists = this.results.some(result => result.elementText === elementText);
           if (!exists) {
-
-            // If result doesn't already exist, create result object
-            var result = new Result({
-              elementText: elementText,
-              outerIndex: outerIndex,
-              headerIndex: headerIndex,
-              resultBelow: null,
-              scrollTarget: eventHeaders[headerIndex].closest('.timeline-event'),
-              elements: {
-                headerElement: eventHeaders[headerIndex], 
-                rightBranchLabel: eventHeaders[headerIndex].closest('.right-branch-label'), 
-                eventLine: eventHeaders[headerIndex].closest('.event-line'),
-              }
-            })
-
-            // Append new result object to result list
+            // Append new result object to searchEngine result array
             this.results.push(result)
-
           }
 
+          // Flag result as positive query match
+          result.positive = true;
+
+          // Append new result object to month objects events array
+          month.events.push(result);
+        }
+
+        // If result does not match query
+        else {
+          // Append new result object to month objects positive results array
+          month.events.push(result);
         }
           
-      } 
-
-      // Check if month outer has any matching results
-      this.results.forEach((result, index) => {
-
-        // Check if result is from current month outer
-        if (result.outerIndex == outerIndex) {
-          // Reset any styling that other searches may have applied
-          container.style.opacity = "100%";
-        }
-
-        // If month outer has no resuls within it, set opacity to 50%
-        else {
-          container.style.opacity = "50%";
-        }
-  
-      });
+      }
+      
+      // Add month to searchEngine month list
+      this.months.push(month);
 
     }
 
+    // Set styling for each month block
+    this.months.forEach(month => {
+      month.checkResultsBelow();
+      month.setStyle();
+    });
 
   }
 
@@ -112,48 +260,8 @@ class SearchEngine {
 
   }
 
-
-  
 }
 
-
-class Result {
-  constructor({
-    elementText, 
-    outerIndex, 
-    headerIndex, 
-    resultBelow, 
-    scrollTarget,
-    elements,
-  }) {
-    this.elementText = elementText;
-    this.outerIndex = outerIndex;
-    this.headerIndex = headerIndex;
-    this.resultBelow = resultBelow;
-    this.scrollTarget = scrollTarget;
-    this.elements = elements;
-  }
-
-  /**
-    * Method to style result object
-    */
-  styleResult(result) {
-
-    result.elements["headerElement"].style.opacity = "100%";
-
-  }
-
-  /**
-  * Method to style negative result object
-  */
-  styleNegativeResult(result) {
-
-    result.elements["headerElement"].style.opacity = "50%";
-
-  }
-
-
-}
 
 
 
