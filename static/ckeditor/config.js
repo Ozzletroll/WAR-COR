652c40da864a70a5f3b46727eed266d6296a168f
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-09-08 22:27:45 +0100

    Adjust ckeditor field styling

--- static/ckeditor/config.js
+++ static/ckeditor/config.js
@@ -35,4 +35,8 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
+
+	// Fonts
+	config.contentsCss = '/static/css/styles.css';
+
 };
