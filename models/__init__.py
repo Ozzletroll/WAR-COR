93adfa29bd759ddc38942c65504f87a1df090276
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-10-02 11:55:49 +0100

    Refactor models into separate files within module

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+from models.associations import *
+from models.campaign import Campaign
+from models.comment import Comment
+from models.epoch import Epoch
+from models.event import Event
+from models.message import Message
+from models.user import User
