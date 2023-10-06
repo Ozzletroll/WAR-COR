f9da8522b3d333ac2b22b8a6cc2ebabbbc6d05fd
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-10-06 13:36:43 +0100

    Add placeholder contact and about pages

--- routes/home/routes.py
+++ routes/home/routes.py
@@ -10,12 +10,21 @@ from routes.home import bp
 # Main page
 @bp.route("/")
 def home():
-
     # Get year for page footer
     date = datetime.date.today()
     year = date.year
 
     return render_template("index.html", year=year)
+
+
+@bp.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@bp.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 # Get and clear campaign page scroll target session variable
