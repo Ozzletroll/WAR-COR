bf2c9104c7d1c4ed3b5d1d16c84850bd030e0899
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-10-23 14:22:24 +0100

    Add error tests

---
+++ tests/test_error.py
@@ -0,0 +1,25 @@
+from sqlalchemy import select
+from app import db
+import models
+import forms
+
+
+# Test 403 page
+def test_access_denied_error(client):
+    response = client.get("/test/restricted_route", follow_redirects=True)
+    assert response.status_code == 403
+    assert b'<h3 class="campaigns-heading">ERROR 403 ACCESS DENIED</h3>' in response.data
+
+
+# Test 404 page
+def test_not_found_error(client):
+    response = client.get("/test/nonexistent_route")
+    assert response.status_code == 404
+    assert b'<h3 class="campaigns-heading">ERROR 404 NOT FOUND</h3>' in response.data
+
+
+# Test 500 page
+def test_internal_error(client):
+    response = client.get("/test/server_error_route")
+    assert response.status_code == 500
+    assert b'<h3 class="campaigns-heading">ERROR 500 INTERNAL SERVER ERROR</h3>' in response.data
