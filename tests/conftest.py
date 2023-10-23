740bdeb672e33208005897a361a1241e43172dbb
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-10-23 15:45:44 +0100

    Add user auth tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,9 +10,36 @@ def app():
 
 @pytest.fixture()
 def client(app):
-    # app.config['WTF_CSRF_ENABLED'] = False
     app.config["TESTING"] = True
     app.config['SECRET_KEY'] = "TESTING_KEY"
     app.config['WTF_CSRF_ENABLED'] = False
     with app.test_client() as client:
         yield client
+
+
+@pytest.fixture()
+def auth(client):
+    return AuthActions(client)
+
+
+class AuthActions(object):
+    """ Class to facilitate common user auth functions during testing """
+
+    def __init__(self, client):
+        self._client = client
+
+    def login(self, username="test", password="test"):
+        return self._client.post("/login", follow_redirects=True, data={
+            "username": username,
+            "password": password,
+        })
+
+    def register(self, username='test', password='test'):
+        return self._client.post("/register", follow_redirects=True, data={
+            "username": username,
+            "password": password,
+            "confirm_password": password,
+        })
+
+    def logout(self):
+        return self._client.get("/logout", follow_redirects=True)
---
+++ tests/test_user.py
@@ -0,0 +1,51 @@
+from sqlalchemy import select
+from flask_login import current_user
+import werkzeug
+from werkzeug.security import generate_password_hash
+
+import models
+from app import db
+
+TEST_USERNAME = "test_user"
+TEST_PASSWORD = "123"
+
+
+def test_register(client, app, auth):
+    # Test if a new user can be added
+    response = auth.register(username=TEST_USERNAME,
+                             password=TEST_PASSWORD)
+    assert response.status_code == 200
+
+    # Check that the new user was added to the database
+    user_query = db.session.execute(select(models.User).filter_by(username=TEST_USERNAME)).scalar()
+    assert user_query.username == TEST_USERNAME
+    assert werkzeug.security.check_password_hash(pwhash=user_query.password, password=TEST_PASSWORD)
+
+
+def test_login(client, auth):
+    response = auth.login(username=TEST_USERNAME,
+                          password=TEST_PASSWORD)
+    assert response.status_code == 200
+    assert current_user.username == TEST_USERNAME
+
+
+def test_logout(client, auth):
+    # Login user
+    auth.login(username=TEST_USERNAME,
+               password=TEST_PASSWORD)
+
+    # Test if the user can be logged out
+    response = auth.logout()
+    assert response.status_code == 200
+    assert current_user.is_anonymous is True
+
+
+def test_user_page(client, app, auth):
+    # Login user
+    # Login user
+    auth.login(username=TEST_USERNAME,
+               password=TEST_PASSWORD)
+
+    # Test if the user page accessible
+    response = client.get(f"/user/{TEST_USERNAME}", follow_redirects=True)
+    assert b'<h3 class="user-heading heading-line-2">test_user</h3>' in response.data
