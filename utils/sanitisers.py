8e72943935193be806ab3245543c26fd48dc1b53
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-11-04 15:39:41 +0000

    Refactor sanitising methods as helper function

---
+++ utils/sanitisers.py
@@ -0,0 +1,23 @@
+import bleach
+from bleach.css_sanitizer import CSSSanitizer
+
+
+def sanitise_input(value):
+    """ Method to sanitise user submitted html input. Uses Bleach
+        and CSSSanitizer. """
+    
+    allowed_tags = ["p", "b", "em", "h1", "h2", "h3", "a", "br", "u", "img", "ul", "ol"]
+    allowed_attrs = {
+        "*": ["class"],
+        "a": ["href", "rel"],
+        "img": ["alt", "src", "style"],
+        }
+    allowed_styles = ["height", "width"]
+    
+    css_santizer = CSSSanitizer(allowed_css_properties=allowed_styles)
+    value = bleach.clean(value, 
+                        tags=allowed_tags,
+                        attributes=allowed_attrs,
+                        css_sanitizer=css_santizer)
+    
+    return value
