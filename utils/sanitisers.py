06a790766105fb469ce1accae9c04b360f4c78b0
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-11-06 15:07:34 +0000

    Add all summernote html elements to allowed list

--- utils/sanitisers.py
+++ utils/sanitisers.py
@@ -6,13 +6,14 @@ def sanitise_input(value):
     """ Method to sanitise user submitted html input. Uses Bleach
         and CSSSanitizer. """
     
-    allowed_tags = ["p", "b", "em", "h1", "h2", "h3", "a", "br", "u", "img", "ul", "ol"]
+    allowed_tags = ["p", "b", "em", "h1", "h2", "h3", "a", "br", "u", "img", "li", "ul", "ol"]
     allowed_attrs = {
         "*": ["class"],
         "a": ["href", "rel"],
         "img": ["alt", "src", "style"],
+        "p": ["align", "style"],
         }
-    allowed_styles = ["height", "width"]
+    allowed_styles = ["height", "width", "margin-left"]
     
     css_santizer = CSSSanitizer(allowed_css_properties=allowed_styles)
     value = bleach.clean(value, 
