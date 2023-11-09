57a996b495ea0f99ea66bbae583816465c7a61dd
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-11-09 22:17:22 +0000

    Add test_leave_comment

--- tests/conftest.py
+++ tests/conftest.py
@@ -145,3 +145,13 @@ class EventActions(object):
                       campaign_id=campaign_object.id)
 
         return self._client.post(url, data=data, follow_redirects=True)
+
+    def create_comment(self, campaign_object, event_object, data):
+
+        url = url_for("event.view_event",
+                      campaign_name=campaign_object.title,
+                      campaign_id=campaign_object.id,
+                      event_name=event_object.title,
+                      event_id=event_object.id)
+
+        return self._client.post(url, data=data, follow_redirects=True)
