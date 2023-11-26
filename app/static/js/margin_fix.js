e6f799212149e31a87a8cd685f866fe30928cab0
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-11-26 23:10:49 +0000

    Remove debug messages

--- app/static/js/margin_fix.js
+++ app/static/js/margin_fix.js
@@ -4,17 +4,11 @@ function marginFix() {
     var is_safari = navigator.userAgent.indexOf("Safari") > -1 && navigator.userAgent.indexOf('Chrome') == -1;
 
     if (is_safari) {
-        console.log("Safari")
         var timelines = document.getElementsByClassName("timeline-year-container");
         var timelineWidth = timelines[0].offsetWidth;
-        
         for (var i = 0; i < timelines.length; i++) {
             timelines[i].style.marginRight = -0.5 * timelineWidth + "px";
         }
-
-    }
-    else {
-        console.log("Not Safari")
     }
 
 }
