db3937e8b9bca1f7893e89b1180233de9caf8812
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-11-28 17:06:14 +0000

    Add character count to campaign description field

---
+++ app/static/js/char_count.js
@@ -0,0 +1,18 @@
+const charField = document.getElementsByClassName("note-editable")[0];
+const charCount = document.getElementById("remaining-chars");
+const charLimit = parseInt(charCount.innerText);
+
+function updateCharCount(charField) {
+
+    var currentChars = charField.textContent.length;
+    var remainingChars = charLimit - currentChars;
+    charCount.innerText = remainingChars;
+    
+}
+
+updateCharCount(charField);
+
+// Add event listener to the input field
+charField.addEventListener("input", function() {
+  updateCharCount(charField);
+});
