8d24f52a575fe034b5a8a5ce2f86f9074d98717e
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-12-15 22:08:55 +0000

    Fix missing config variables for email logging

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import os
 
 
 class Config(object):
-
     SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = "sqlite:///war_cor.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -11,10 +10,10 @@ class Config(object):
     SESSION_COOKIE_SAMESITE = "Lax"
     MAIL_SERVER = os.environ.get("MAIL_SERVER")
     MAIL_PORT = int(os.environ.get("MAIL_PORT") or 25)
-    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS")
+    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    ADMINS = ["admin-email-goes-here"]
+    ADMIN = os.environ.get("ADMIN")
 
 
 class TestingConfig(Config):
