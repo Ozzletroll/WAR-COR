264e4de7fd43148d808cf2f5a00df27debe2f617
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-12-20 14:28:00 +0000

    Remake hamburger menu as own class

--- app/static/js/navbar.js
+++ app/static/js/navbar.js
@@ -1,28 +1,44 @@
 
-// Continously check screen size
-function handleResize() {
-  let newHeight = window.innerHeight;
-  let newWidth = window.innerWidth;
-  var menu = document.getElementById("hamburgerMenu");
+class hamburgerMenu {
+  constructor({
+    button,
+    menu,
+  }) {
+    this.button = document.getElementById(button);
+    this.menu = document.getElementById(menu);
+    this.state = false;
+    this.button.addEventListener("click", this.toggleMenu.bind(this));
+    window.addEventListener("resize", this.handleResize.bind(this));
+    this.handleResize();
+  }
 
-  // Hide hamburger menu if screen has been resized beyond 700px
-  if (newWidth >= 700) {
-    menu.style.display = "none"
+  toggleMenu () {
+
+    if (this.state == false) {
+      this.menu.style.transform = "translateY(0)";
+      this.state = true;
+    } 
+    else {
+      this.menu.style.transform = "";
+      this.state = false;
+    }
+  }
+
+  handleResize() {
+
+    // Hide hamburger menu if screen has been resized beyond 700px
+    var newWidth = window.innerWidth;
+    if (newWidth >= 700) {
+      this.menu.style.display = "none";
+    }
+    else {
+      this.menu.style.display = "flex";
+    }
   }
 
 }
 
-window.addEventListener("resize", handleResize);
-
-// calling the resize function for the first time
-handleResize();
-
-// Display the hamburger menu on click
-function hamburgerClick() {
-  var menu = document.getElementById("hamburgerMenu");
-  if (menu.style.display === "flex") {
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "flex";
-  }
-}
+const hamburger = new hamburgerMenu({
+  button: "hamburger-button",
+  menu: "hamburger-menu",
+})
