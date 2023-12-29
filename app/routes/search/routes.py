a1a643f08b4035c211b57e569713871f47143ca1
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2023-12-29 15:36:27 +0000

    Remake advanced_search to allow viewing edit pages

--- app/routes/search/routes.py
+++ app/routes/search/routes.py
@@ -1,10 +1,10 @@
-from flask import render_template, redirect, request, url_for, flash
+from flask import render_template, redirect, request, url_for, flash, session
+from flask_login import current_user
 from sqlalchemy import select
-from app.forms import forms
-from app import models
-import app.utils.search as search
 
-from app import db
+from app.forms import forms
+from app import models, db
+import app.utils.search as search
 from app.routes.search import bp
 
 
@@ -15,23 +15,23 @@ from app.routes.search import bp
 # Advanced search page, accessed from deployable searchbar on timeline
 @bp.route("/campaigns/<campaign_name>-<campaign_id>/search", methods=["GET", "POST"])
 def advanced_search(campaign_name, campaign_id):
+
     campaign = db.session.execute(
         select(models.Campaign)
         .filter_by(id=campaign_id)).scalar()
 
     form = forms.SearchForm()
 
+    # Set back button route
+    excluded_urls = ["/search", "/event"]
+    if request.referrer and not any(excluded_url in request.referrer for excluded_url in excluded_urls):
+        session["previous_url"] = request.referrer
+        
     # Check if page has any results to render
     if "results" not in request.args:
         results = None
     else:
         results = request.args["results"]
-
-    # Check if user came from the edit timeline page
-    if "edit" in request.args:
-        edit = bool(request.args["edit"])
-    else:
-        edit = False
 
     # If form submitted, search database
     if form.validate_on_submit():
@@ -44,30 +44,22 @@ def advanced_search(campaign_name, campaign_id):
 
         if len(results) == 0:
             flash("No results found")
-            if edit:
-                return redirect(url_for("search.advanced_search",
-                                        campaign_name=campaign.title,
-                                        campaign_id=campaign.id,
-                                        edit=edit))
-            else:
-                return redirect(url_for("search.advanced_search",
-                                        campaign_name=campaign.title,
-                                        campaign_id=campaign.id))
+            return redirect(url_for("search.advanced_search",
+                                    campaign_name=campaign.title,
+                                    campaign_id=campaign.id))
         else:
-            if edit:
-                return render_template("advanced_search.html",
-                                       form=form,
-                                       campaign=campaign,
-                                       edit=edit,
-                                       results=results)
-            else:
-                return render_template("advanced_search.html",
-                                       form=form,
-                                       campaign=campaign,
-                                       results=results)
+            edit = False
+            if current_user.is_authenticated:
+                if campaign in current_user.permissions:
+                    edit = True
+
+            return render_template("advanced_search.html",
+                                    form=form,
+                                    campaign=campaign,
+                                    results=results,
+                                    edit=edit)
 
     return render_template("advanced_search.html",
                            form=form,
                            campaign=campaign,
-                           edit=edit,
                            results=results)
