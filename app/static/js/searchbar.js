8dd4f4f4e051397028063881d98bf7d73549b408
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-01-02 15:56:26 +0000

    Fix searchbar width on mobile devices

--- app/static/js/searchbar.js
+++ app/static/js/searchbar.js
@@ -39,7 +39,7 @@ class SearchbarTab {
   getWidth() {
     if (window.innerWidth <= 600 && this.state == true) {
       this.outer.style.justifyContent = "space-between";
-      this.tab.style.width = "100vw";
+      this.tab.style.width = "100%";
       this.area.style.flex = "4 1 0";
       this.area.style.marginLeft = "10px";
       this.areaInner.style.width = "100%";
