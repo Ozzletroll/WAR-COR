29ef8049f91f31acea72caca0a848a349a7b7786
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-01-08 19:27:41 +0000

    Adjust user image styling in campaign description

---
+++ app/static/js/user_image_styling.js
@@ -0,0 +1,31 @@
+// Apply styling to user submitted image links
+const elements = document.querySelectorAll(".campaign-overview p");
+let imageCount = 0;
+
+elements.forEach((element) => {
+  if (element.querySelector("img")) {
+    imageCount++;
+
+    // Create surrounding element
+    const newDiv1 = document.createElement("div");
+    newDiv1.classList.add("user-image-area");
+
+    // Wrap the element within the new div
+    element.parentNode.insertBefore(newDiv1, element);
+    newDiv1.appendChild(element);
+
+    // Create surrounding element
+    const newDiv2 = document.createElement("div");
+    newDiv2.classList.add("user-image-elem")
+    newDiv2.classList.add("campaign-image-elem");;
+
+    // Wrap the element within the new div
+    element.parentNode.insertBefore(newDiv2, element);
+    newDiv2.appendChild(element);
+
+    // Centre img elements in user submitted p elements
+    element.style.display = "flex";
+    element.style.justifyContent = "center";
+    element.style.margin = "0";
+
+  }});
