da6ce32f51d7baa0b8e28e5cf46b457fc98f39cb
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-01-23 17:13:06 +0000

    Add html preview to epoch desc field

--- app/static/js/epoch_form.js
+++ app/static/js/epoch_form.js
@@ -30,6 +30,96 @@ class Modal {
 }
 
 
+// Preview Area Modal class
+class PreviewModal {
+  constructor({
+    modal,
+    button,
+    span,
+  }) {
+    this.modal = document.getElementById(modal);
+    this.button = button;
+    this.span = span;
+
+    document.getElementById(this.button).onclick = event => {
+      this.openModal(event)
+    } 
+
+    document.getElementById(this.span).onclick = event => {
+      this.closeModal(event)
+    } 
+
+  }
+  
+  openModal() {
+    this.modal.style.display = "flex";
+    this.htmlPreview()
+  }
+
+  closeModal() {
+    this.modal.style.display = "none";
+  }
+
+  htmlPreview() {
+
+    // Get the content of the Summernote
+    var editor = document.querySelector("#epoch-desc-field .note-editable");
+    var content = editor.innerHTML;
+
+    // Get the preview modal text area and add html
+    var modalBody = document.getElementById("preview-modal-body");
+    modalBody.innerHTML = content;
+   
+    this.formatImages();
+  
+  }
+
+  formatImages() {
+    // Apply styling to user submitted image links
+    const elements = document.querySelectorAll(".modal-body-preview p");
+    let imageCount = 0;
+
+    elements.forEach((element) => {
+      if (element.querySelector("img")) {
+        imageCount++;
+
+        // Create surrounding element
+        const newDiv1 = document.createElement("div");
+        newDiv1.classList.add("user-image-area");
+
+        // Wrap the element within the new div
+        element.parentNode.insertBefore(newDiv1, element);
+        newDiv1.appendChild(element);
+
+        // Create surrounding element
+        const newDiv2 = document.createElement("div");
+        newDiv2.classList.add("user-image-elem");
+
+        // Wrap the element within the new div
+        element.parentNode.insertBefore(newDiv2, element);
+        newDiv2.appendChild(element);
+
+        // Create header
+        const newHeader = document.createElement("div");
+        newHeader.classList.add("user-image-header");
+        newHeader.textContent = `Image::Data_${String(imageCount).padStart(2, "0")}`;
+
+        // Insert the header
+        element.parentNode.insertBefore(newHeader, element);
+
+        // Centre img elements in user submitted p elements
+        element.style.display = "flex";
+        element.style.width = "auto";
+        element.style.justifyContent = "center";
+        element.style.margin = "0";
+
+      }});
+
+  }
+
+}
+
+
 // Create modals
 const modal_1 = new Modal({
   modal: "modal-1",
@@ -55,6 +145,14 @@ const modal_4 = new Modal({
   span: "close-4",
 })
 
+// HTML Preview Area Modal
+const modal_preview = new PreviewModal({
+  modal: "modal-preview",
+  button: "html-preview-button",
+  span: "close-preview",
+})
+
+
 // Only create delete modal if elements present (IE, we are actually on the edit epoch template)
 const modalDeleteTest = document.getElementById("modal-delete")
 if (modalDeleteTest != null) {
@@ -64,7 +162,6 @@ if (modalDeleteTest != null) {
     span: "close-delete",
   })
 }
-
 
 // Close modals if the user clicks anywhere else
 window.onclick = function(event) {
@@ -79,6 +176,9 @@ window.onclick = function(event) {
   }
   if (event.target == modal_4.modal) {
     modal_4.closeModal();
+  }
+  if (event.target == modal_preview.modal) {
+    modal_preview.closeModal();
   }
   if (modalDeleteTest != null) {
     if (event.target == modal_delete.modal) {
