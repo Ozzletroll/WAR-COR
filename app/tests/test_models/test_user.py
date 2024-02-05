b3cbc6210e02272be61ca781fd4ffd72a4a0e914
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-02-05 19:32:26 +0000

    Add test_change_password

--- app/tests/test_models/test_user.py
+++ app/tests/test_models/test_user.py
@@ -19,3 +19,31 @@ def test_update(client):
     assert werkzeug.security.check_password_hash(
         pwhash=user.password,
         password="Password")
+
+
+def test_change_password(client):
+
+    user = models.User()
+    user_form = {
+        "email": "email@testemail.com",
+        "username": "New Username",
+        "password": "Password"
+    }
+    user.update(user_form, new=True)
+
+    change_password_form = {
+        "old_password": "Password",
+        "new_password": "NewPassword"
+    }
+    user.change_password(change_password_form)
+    assert werkzeug.security.check_password_hash(
+        pwhash=user.password,
+        password="NewPassword")
+
+    change_password_form_reset = {
+        "new_password": "ResetPassword"
+    }
+    user.change_password(change_password_form_reset, reset=True)
+    assert werkzeug.security.check_password_hash(
+        pwhash=user.password,
+        password="ResetPassword")
