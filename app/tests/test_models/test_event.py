f1a5decb31809b5deb6b07d90037172634138fd9
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-02-05 17:53:01 +0000

    Add test_separate_belligerents

--- app/tests/test_models/test_event.py
+++ app/tests/test_models/test_event.py
@@ -58,3 +58,32 @@ def test_split_date(client):
     assert event.hour == 12
     assert event.minute == 0
     assert event.second == 0
+
+
+def test_separate_belligerents(client):
+
+    event_1 = models.Event()
+    event_1.belligerents = "Group 1 & Group 2, Group 3"
+
+    belligerents_1 = event_1.separate_belligerents()
+    # Assert two vs groups
+    assert len(belligerents_1) == 2
+    # Assert group's 1 and 2 in first list
+    assert len(belligerents_1[0]) == 2
+    assert belligerents_1[0][0] == "Group 1"
+    assert belligerents_1[0][1] == "Group 2"
+    # Assert group 2 in second list
+    assert len(belligerents_1[1]) == 1
+    assert belligerents_1[1][0] == "Group 3"
+
+    event_2 = models.Event()
+    event_2.belligerents = "Group 1, Group 2, Group 3, Group 4"
+    belligerents_2 = event_2.separate_belligerents()
+    # Assert four vs groups of 1 belligerent
+    assert len(belligerents_2) == 4
+    for group in belligerents_2:
+        assert len(group) == 1
+    assert belligerents_2[0][0] == "Group 1"
+    assert belligerents_2[1][0] == "Group 2"
+    assert belligerents_2[2][0] == "Group 3"
+    assert belligerents_2[3][0] == "Group 4"
