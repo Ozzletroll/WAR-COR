9b7e007bc1c19e0a233fe5c8fad16a5d4a3912d0
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-03-07 15:57:29 +0000

    Move PreviewModal to Modal module

--- app/static/js/modal.js
+++ app/static/js/modal.js
@@ -41,3 +41,79 @@ export class Modal {
   
   }
   
+
+  // Preview Area Modal class
+export class PreviewModal extends Modal{
+  constructor({
+    modal,
+    button,
+    span,
+  }) {
+    super({ modal, button, span });
+
+    this.button.onclick = event => {
+      this.openModal(event)
+      this.htmlPreview()
+    } 
+
+    this.span.onclick = event => {
+      this.closeModal(event)
+    } 
+
+  }
+  
+  htmlPreview() {
+
+    // Get the content of the Summernote editor and add it to the modal
+    var editor = document.querySelector('.note-editable');
+    var content = editor.innerHTML;
+    var modalBody = document.getElementById("preview-modal-body");
+    modalBody.innerHTML = content;
+   
+    this.formatImages();
+  }
+
+  formatImages() {
+    // Apply styling to user submitted image links
+    const elements = document.querySelectorAll(".modal-body-preview p");
+    let imageCount = 0;
+
+    elements.forEach((element) => {
+      if (element.querySelector("img")) {
+        imageCount++;
+
+        // Create surrounding element
+        const newDiv1 = document.createElement("div");
+        newDiv1.classList.add("user-image-area");
+
+        // Wrap the element within the new div
+        element.parentNode.insertBefore(newDiv1, element);
+        newDiv1.appendChild(element);
+
+        // Create surrounding element
+        const newDiv2 = document.createElement("div");
+        newDiv2.classList.add("user-image-elem");
+
+        // Wrap the element within the new div
+        element.parentNode.insertBefore(newDiv2, element);
+        newDiv2.appendChild(element);
+
+        // Create header
+        const newHeader = document.createElement("div");
+        newHeader.classList.add("user-image-header");
+        newHeader.textContent = `Image::Data_${String(imageCount).padStart(2, "0")}`;
+
+        // Insert the header
+        element.parentNode.insertBefore(newHeader, element);
+
+        // Centre img elements in user submitted p elements
+        element.style.display = "flex";
+        element.style.width = "auto";
+        element.style.justifyContent = "center";
+        element.style.margin = "0";
+
+      }});
+
+  }
+
+}
