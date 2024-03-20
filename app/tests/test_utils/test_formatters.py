fdaf0160c2bbc313d184783d287795971a5b62db
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-03-20 16:34:29 +0000

    Add test_increment_datestring

--- app/tests/test_utils/test_formatters.py
+++ app/tests/test_utils/test_formatters.py
@@ -14,3 +14,34 @@ def test_split_date():
     test_string_3 = "5016/01/01"
     output_3 = formatters.split_date(test_string_3, epoch_date=True)
     assert output_3 == [5016, 1, 1]
+
+
+def test_increment_datestring():
+
+    test_string_1 = "5016/01/01 10:00:00"
+    output_1 = formatters.increment_datestring(test_string_1, args={"new_month": True})
+    assert output_1 == "5016/02/01 00:00:00"
+
+    test_string_2 = "5016/99/01 10:00:00"
+    output_2 = formatters.increment_datestring(test_string_2, args={"new_month": True})
+    assert output_2 == "5017/01/01 00:00:00"
+
+    test_string_3 = "5016/01/01 10:00:00"
+    output_3 = formatters.increment_datestring(test_string_3, args={"new_day": True})
+    assert output_3 == "5016/01/02 00:00:00"
+
+    test_string_4 = "5016/01/99 10:00:00"
+    output_4 = formatters.increment_datestring(test_string_4, args={"new_day": True})
+    assert output_4 == "5016/02/01 00:00:00"
+
+    test_string_5 = "5016/01/01 10:00:00"
+    output_5 = formatters.increment_datestring(test_string_5, args={"new_hour": True})
+    assert output_5 == "5016/01/01 11:00:00"
+
+    test_string_6 = "5016/01/01 99:00:00"
+    output_6 = formatters.increment_datestring(test_string_6, args={"new_hour": True})
+    assert output_6 == "5016/01/02 01:00:00"
+
+    test_string_6 = "5016/99/99 99:00:00"
+    output_6 = formatters.increment_datestring(test_string_6, args={"new_hour": True})
+    assert output_6 == "5017/01/01 01:00:00"
