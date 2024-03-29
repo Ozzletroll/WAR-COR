dfacf587ad10fa330f7fba8c81b172c967357f0a
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-03-29 17:05:48 +0000

    Refactor tab mobile script into separate script

---
+++ app/static/js/campaign_mobile_tabs.js
@@ -0,0 +1,42 @@
+// Create mobile tabs for all campaigns
+const campaigns = [];
+var tabs = document.getElementsByClassName("campaign-entry");
+Array.from(tabs).forEach((element) => {
+
+  if (element.classList.contains("campaign-new-user-entry")) {
+    return
+  }
+  else {
+    var buttonElement = element.querySelector(".campaign-header");
+    var tabElement = element.querySelector(".campaign-body");
+  
+    var tab = new Tab({
+      tab: tabElement,
+      button: buttonElement,
+    })
+  
+    campaigns.push(tab);
+  }
+
+})
+
+// If resizing above 500px, open all tabs again
+window.addEventListener("resize", function() {
+  if (window.innerWidth > 500) {
+    Array.from(campaigns).forEach((tab) => {
+      tab.openTab();
+    })
+  }
+  else {
+    Array.from(campaigns).forEach((tab) => {
+      tab.closeTab();
+      tab.checkStatus();
+    })
+  }
+});
+
+window.addEventListener("DOMContentLoaded", function() {
+  Array.from(campaigns).forEach((tab) => {
+    tab.checkStatus();
+  })
+});
