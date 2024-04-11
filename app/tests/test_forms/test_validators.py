3ea7969eb1a939d72788eafbbdc626d3bdf4deea
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-04-11 16:48:45 +0100

    Refactor validate fixture to mock form

--- app/tests/test_forms/test_validators.py
+++ app/tests/test_forms/test_validators.py
@@ -1,26 +1,28 @@
 from app.forms.validators import *
+from app.forms import forms
 import pytest
+from unittest.mock import MagicMock
 
 
 @pytest.fixture
 def validate_fixture():
-    def _validate(validator, form_data, field_data):
-        class MockForm:
-            data = form_data
+    def _validate(validator, form_class, form_data, field_data):
 
-        class MockField:
-            data = field_data
+        form = MagicMock(spec=form_class)
+        for value in form_data:
+            field = MagicMock()
+            field.data = form_data[value]
+            setattr(form, value, field)
 
-        form = MockForm()
-        field = MockField()
+        current_field = MagicMock()
+        current_field.data = field_data
 
-        validator(form, field)
+        validator(form, current_field)
 
     return _validate
 
 
 def test_date_format(validate_fixture):
-
     correct_event_format = "5016/01/01 12:00:00"
     correct_epoch_format = "5016/01/01"
     incorrect_event_format = "5016/1/01 12:00:00"
@@ -29,59 +31,67 @@ def test_date_format(validate_fixture):
     # Test that incorrect formats raise ValidationErrors
     with pytest.raises(ValidationError):
         validate_fixture(date_format("event"),
+                         forms.CreateEventForm,
                          {"date": incorrect_event_format},
                          incorrect_event_format)
 
     with pytest.raises(ValidationError):
         validate_fixture(date_format("epoch"),
+                         forms.CreateEventForm,
                          {"date": incorrect_epoch_format},
                          incorrect_epoch_format)
 
     # Test correct format passes
     validate_fixture(date_format("event"),
+                     forms.CreateEventForm,
                      {"date": correct_event_format},
                      correct_event_format)
 
     validate_fixture(date_format("epoch"),
+                     forms.CreateEventForm,
                      {"date": correct_epoch_format},
                      correct_epoch_format)
 
 
 def test_file_format(validate_fixture):
-
     # Test that 6MB file raises ValidationError
     with pytest.raises(ValidationError):
         validate_fixture(file_format(),
+                         forms.UploadJsonForm,
                          {},
                          type("obj", (object,), {"content_length": 6 * 1024 * 1024}))
 
     # Test that 3MB file passes
     validate_fixture(file_format(),
+                     forms.UploadJsonForm,
                      {},
                      type("obj", (object,), {"content_length": 3 * 1024 * 1024}))
 
 
 def test_image_url(validate_fixture):
-
     no_image_url = ""
     broken_image_url = "asd"
     non_image_url = "https://www.google.com/"
     correct_image_url = "https://massifpress.com/_next/image?url=%2Fimages%2Flancer%2Flancer-carousel.webp&w=1920&q=75"
 
     validate_fixture(image_url(),
+                     forms.CreateCampaignForm,
                      {},
                      no_image_url)
 
     with pytest.raises(ValidationError):
         validate_fixture(image_url(),
+                         forms.CreateCampaignForm,
                          {},
                          broken_image_url)
 
     with pytest.raises(ValidationError):
         validate_fixture(image_url(),
+                         forms.CreateCampaignForm,
                          {},
                          non_image_url)
 
     validate_fixture(image_url(),
+                     forms.CreateCampaignForm,
                      {},
                      correct_image_url)
