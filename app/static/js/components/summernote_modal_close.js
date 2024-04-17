785b205fa4499d636e80785987606b2f4e716872
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-04-17 16:43:19 +0100

    Add script to close summernote modals on bg click

---
+++ app/static/js/components/summernote_modal_close.js
@@ -0,0 +1,16 @@
+
+const summernoteModals = document.getElementsByClassName("note-modal");
+
+Array.from(summernoteModals).forEach(modal => {
+    var modalCloseButton = modal.querySelector(".close");
+
+    modal.addEventListener("click", (event) => {
+        if (event.target.classList.contains("note-modal")) {
+            closeModal(modalCloseButton);
+        }
+    });
+});
+
+function closeModal(modalCloseButton) {
+    modalCloseButton.click();
+}
