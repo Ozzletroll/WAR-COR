2480f205b9fa14c2620ee57b7a5929ad0cdeb143
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-04-30 20:43:58 +0100

    Fix error test assertions

--- app/tests/test_routes/test_error.py
+++ app/tests/test_routes/test_error.py
@@ -2,18 +2,18 @@
 def test_access_denied_error(client):
     response = client.get("/test/restricted_route", follow_redirects=True)
     assert response.status_code == 403
-    assert b'<h3 class="campaigns-heading">ERROR 403 Forbidden</h3>' in response.data
+    assert b'<h3 class="campaigns-heading">403 Forbidden</h3>' in response.data
 
 
 # Test 404 page
 def test_not_found_error(client):
     response = client.get("/test/nonexistent_route")
     assert response.status_code == 404
-    assert b'<h3 class="campaigns-heading">ERROR 404 Not Found</h3>' in response.data
+    assert b'<h3 class="campaigns-heading">404 Not Found</h3>' in response.data
 
 
 # Test 500 page
 def test_internal_error(client):
     response = client.get("/test/server_error_route")
     assert response.status_code == 500
-    assert b'<h3 class="campaigns-heading">ERROR 500 Internal Server Error</h3>' in response.data
+    assert b'<h3 class="campaigns-heading">500 Internal Server Error</h3>' in response.data
