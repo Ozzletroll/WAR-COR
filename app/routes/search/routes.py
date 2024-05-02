701e47089d225509ca504f27d05e05e883fcdcbe
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-05-02 21:38:53 +0100

    Update advanced_search to use pagination

--- app/routes/search/routes.py
+++ app/routes/search/routes.py
@@ -3,7 +3,7 @@ from flask_login import current_user
 
 from app.forms import forms
 from app import db, models
-import app.utils.search as search
+import app.utils.search as search_tools
 
 from app.routes.search import bp
 
@@ -22,45 +22,71 @@ def advanced_search(campaign_name, campaign_id):
 
     form = forms.SearchForm()
 
+    page = request.args.get("page", 1, type=int)
+    per_page = 10
+    search_engine = search_tools.SearchEngine()
+
     # Set back button route, excluding this route
     if not session["previous_url"]:
         session["previous_url"] = url_for("campaign.show_timeline", 
                                           campaign_name=campaign.url_title, 
                                           campaign_id=campaign.id)
-    
-    # Check if page has any results to render
-    if "results" not in request.args:
-        results = None
-    else:
-        results = request.args["results"]
 
-    # If form submitted, search database
-    if form.validate_on_submit():
-        search_query = request.form["search"]
+    if request.method == "GET":
 
-        search_engine = search.SearchEngine()
-        search_engine.search_campaign(campaign=campaign,
-                                      query=search_query)
-        results = search_engine.return_results()
-
-        if len(results) == 0:
-            flash("No results found")
-            return redirect(url_for("search.advanced_search",
-                                    campaign_name=campaign.title,
-                                    campaign_id=campaign.id))
+        if "search" not in request.args:
+            paginator = None
+            search = None
         else:
+            search = request.args["search"]
+            form.search.data = search
+
+            search_engine.search_campaign(campaign=campaign,
+                                          query=search)
+            results = search_engine.return_results()
+
             edit = False
             if current_user.is_authenticated:
                 if campaign in current_user.permissions:
                     edit = True
 
-            return render_template("pages/advanced_search.html",
-                                   form=form,
-                                   campaign=campaign,
-                                   results=results,
-                                   edit=edit)
+            paginator = search_tools.Paginator(data=results, page=page, per_page=per_page)
 
-    return render_template("pages/advanced_search.html",
-                           form=form,
-                           campaign=campaign,
-                           results=results)
+        return render_template("pages/advanced_search.html",
+                                form=form,
+                                campaign=campaign,
+                                search=search,
+                                paginator=paginator)
+
+    elif request.method == "POST":
+
+        if form.validate_on_submit():
+
+            page = 1
+            search = request.form["search"].lower()
+            search_engine.search_campaign(campaign=campaign,
+                                        query=search)
+            results = search_engine.return_results()
+            paginator = search_tools.Paginator(data=results, page=page, per_page=per_page)
+
+            if len(results) == 0:
+                flash("No results found")
+                return redirect(url_for("search.advanced_search",
+                                        campaign_name=campaign.title,
+                                        campaign_id=campaign.id))
+            else:
+                edit = False
+                if current_user.is_authenticated:
+                    if campaign in current_user.permissions:
+                        edit = True
+
+                return render_template("pages/advanced_search.html",
+                                       form=form,
+                                       campaign=campaign,
+                                       paginator=paginator,
+                                       search=search,
+                                       edit=edit)
+
+        return redirect(url_for("search.advanced_search",
+                                campaign_name=campaign.title,
+                                campaign_id=campaign.id))
