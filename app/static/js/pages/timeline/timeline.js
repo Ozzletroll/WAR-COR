94b8f0f8fdea5d266a6d24d3cb5fa3cf3dffc432
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-05-23 16:21:41 +0100

    Prevent help modal creation when not on edit layer

--- app/static/js/pages/timeline/timeline.js
+++ app/static/js/pages/timeline/timeline.js
@@ -1,6 +1,7 @@
 import { checkLeftMarker } from '../../components/timeline/timeline_left_markers.js';
 import { adjustEpochLine } from '../../components/timeline/epoch_line_adjust.js';
 import { marginFix } from '../../components/timeline/margin_fix.js';
+import { Modal } from '../../components/modal.js';
 
 // Offset epoch elements
 adjustEpochLine();
@@ -12,3 +13,12 @@ window.addEventListener("DOMContentLoaded", marginFix());
 // Toggle display of left hand year marker
 window.addEventListener("resize", checkLeftMarker);
 checkLeftMarker();
+
+var editPageElem = document.getElementById("editPageVariable").getAttribute("editPage");
+if (editPageElem == "true") {
+  const footerModal = new Modal({
+    modal: document.getElementById("help-modal-1"),
+    button: document.getElementById("footer-help-button"),
+    span: document.getElementById("help-close-1"),
+  })
+}
