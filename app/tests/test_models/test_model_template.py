da2d885b769805d8849415196238d409f26b0396
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-06-03 23:34:35 +0100

    Add test_update

---
+++ app/tests/test_models/test_model_template.py
@@ -0,0 +1,51 @@
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
+import pytest
+from unittest.mock import Mock
+
+from app import db, models
+
+
+def test_setup(client, auth, campaign):
+
+    auth.register()
+    campaign.create(title="Template Test Campaign",
+                    description="A campaign for testing purposes")
+
+
+def test_update(client):
+
+    campaign_object = db.session.execute(
+        select(models.Campaign)
+        .filter_by(title="Template Test Campaign")).scalar()
+
+    template = models.Template(name="New Template",
+                               parent_campaign=campaign_object,
+                               share_code="Test Share Code")
+
+    # Mock the generate_share_code method to return a fixed value
+    template.generate_share_code = lambda: "New Share Code"
+
+    db.session.add(template)
+    db.session.commit()
+
+    # Update the template, generating new share code
+    template.update()
+    assert template.share_code == "New Share Code"
+
+    # Create a second template with a duplicate share code,
+    # expecting an IntegrityError
+    template_2 = models.Template(name="Second Template",
+                                 parent_campaign=campaign_object)
+
+    # Create a mock for the generate_share_code method
+    mock_generate_share_code = Mock()
+    # Set the mock to return "New Share Code" on the first call,
+    # and "Unique Share Code" on the second call
+    mock_generate_share_code.side_effect = ["New Share Code", "Unique Share Code"]
+    template_2.generate_share_code = mock_generate_share_code
+
+    # Call update, assert that the duplicate share code causes it to
+    # generate the second value
+    template_2.update()
+    assert template_2.share_code == "Unique Share Code"
