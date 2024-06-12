40465d9d7fb608701d3b388ae69ddc952ca1dddf
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-06-12 23:01:29 +0100

    Remove redundant validators

--- app/forms/validators.py
+++ app/forms/validators.py
@@ -6,25 +6,7 @@ from urllib.error import HTTPError
 from requests import Session, exceptions
 
 
-def date_format(format):
-
-    if format == "event":
-        message = "Not a valid date format, please use the format 'YYYY/MM/DD HH:MM:SS'"
-        format = r"^-?\d{1,9}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}$"
-    elif format == "epoch":
-        message = "Not a valid date format, please use the format 'YYYY/MM/DD'"
-        format = r"^-?\d{1,9}/\d{2}/\d{2}$"
-
-    def _date_format(form, field):
-
-        if not re.match(format, field.data):
-            raise ValidationError(message)
-
-    return _date_format
-
-
 def file_format():
-
     def _file_format(form, field):
 
         # Check if filesize is below 5MB
@@ -37,30 +19,25 @@ def file_format():
 
 
 def image_url():
-
     def _image_url(form, field):
 
         if not field.data or field.data == "":
             return
-        
+
         allowed_filetypes = ["image/jpg", "image/jpeg", "image/png", "image/gif", "image/webp"]
         session = Session()
 
         try:
             request = session.get(field.data)
-            
         except HTTPError as error:
             if error.code == 403:
                 raise ValidationError("Access to the URL is forbidden")
             else:
                 raise ValidationError("URL must be a valid image link")
-        
         except exceptions.ConnectionError:
             raise ValidationError("Invalid URL. The server could not be reached.")
-
         except exceptions.MissingSchema:
             raise ValidationError("URL must be a valid image link")
-    
         except Exception:
             raise ValidationError("Unforeseen error occurred while validating URL")
 
@@ -72,20 +49,17 @@ def image_url():
 
 
 def date_is_after(form):
-
-    if ((form.start_year.data, form.start_month.data, form.start_day.data) 
-        > (form.end_year.data, form.end_month.data, form.end_day.data)):
-        
+    if ((form.start_year.data, form.start_month.data, form.start_day.data)
+            > (form.end_year.data, form.end_month.data, form.end_day.data)):
         flash("DATE: End Date must be after Start Date")
         return False
-    
+
     return True
 
 
 def plain_text_length(max=600, required=False):
-
     def _plain_text_length(form, field):
-        
+
         plain_text = nh3.clean(field.data, tags=set())
 
         if required and len(plain_text) == 0:
@@ -93,5 +67,5 @@ def plain_text_length(max=600, required=False):
         if max is not None:
             if len(plain_text) > max:
                 raise ValidationError(f"Field must be less than {max} characters")
-        
+
     return _plain_text_length
