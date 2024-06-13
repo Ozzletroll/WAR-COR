e62d3eb3a8894b6c28837dfc2fdebc978b821286
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-06-13 15:10:36 +0100

    Update epoch model tests

--- app/tests/test_models/test_model_epoch.py
+++ app/tests/test_models/test_model_epoch.py
@@ -35,9 +35,13 @@ def test_update(client):
     epoch_form = CreateEpochForm(
         MultiDict({
             "title": "Updated Epoch",
-            "start_date": "5016/01/02",
-            "end_date": "5016/02/02",
-            "overview": "Updated overview",
+            "start_year": 5016,
+            "start_month": 1,
+            "start_day": 2,
+            "end_year": 5016,
+            "end_month": 2,
+            "end_day": 2,
+            "overview": "Overview Text",
             "dynamic_fields-0-title": "Title 1",
             "dynamic_fields-0-value": "Value 1",
             "dynamic_fields-0-is_full_width": False,
@@ -46,7 +50,6 @@ def test_update(client):
             "dynamic_fields-1-value": "Value 2",
             "dynamic_fields-1-is_full_width": False,
             "dynamic_fields-1-field_type": "basic",
-            "hide_time": False,
         })).data
 
     epoch.update(epoch_form,
@@ -55,9 +58,82 @@ def test_update(client):
 
     for field in epoch_form:
         if field == "overview":
-            assert getattr(epoch, field) == "<p>Updated overview</p>"
+            assert getattr(epoch, field) == "<p>Overview Text</p>"
         else:
             assert getattr(epoch, field) == epoch_form[field]
+
+
+def test_map_dynamic_fields():
+    epoch = models.Epoch()
+
+    input_value = [
+        {
+            "title": "Test Title",
+            "value": "<script>virus.exe</script> Text that needs to be sanitised and wrapped in p tags",
+            "field_type": "html",
+            "is_full_width": "incorrect data type"
+        },
+        {
+            "title": "Test Title 2",
+            "value": [
+                {
+                    "entries": [
+                        "ENTRY 1",
+                    ],
+                    "position": "1",
+                    "title": "Allies"
+                },
+                {
+                    "entries": [
+                        "ENTRY 1",
+                    ],
+                    "position": "2",
+                    "title": "Enemies"
+                }
+            ],
+            "field_type": "composite",
+            "is_full_width": True
+        }
+    ]
+
+    expected_output = [
+        {
+            "title": "Test Title",
+            "value": "<p> Text that needs to be sanitised and wrapped in p tags</p>",
+            "field_type": "html",
+            "is_full_width": False
+        },
+        {
+            "title": "Test Title 2",
+            "value": [
+                {
+                    "entries": [
+                        "ENTRY 1",
+                    ],
+                    "position": "1",
+                    "title": "Allies"
+                },
+                {
+                    "entries": [
+                        "ENTRY 1",
+                    ],
+                    "position": "2",
+                    "title": "Enemies"
+                }
+            ],
+            "field_type": "composite",
+            "is_full_width": True
+        }
+    ]
+    output = epoch.map_dynamic_field_data(input_value)
+    assert output == expected_output
+
+
+def test_set_date():
+
+    epoch = models.Epoch()
+    output = epoch.set_date(5016, 1, 5)
+    assert output == "5016/01/05"
 
 
 def test_set_url_title():
@@ -70,6 +146,21 @@ def test_set_url_title():
     assert epoch.url_title == "Test-Epoch"
 
 
+def test_create_blank():
+
+    date_values = [5016, 1, 1]
+
+    epoch = models.Epoch()
+    epoch.create_blank(date_values)
+
+    assert epoch.start_year == 5016
+    assert epoch.start_month == 1
+    assert epoch.start_day == 1
+    assert epoch.end_year == 5016
+    assert epoch.end_month == 1
+    assert epoch.end_day == 1
+
+
 def test_populate_self(client, auth, epoch):
     auth.login()
 
@@ -77,21 +168,23 @@ def test_populate_self(client, auth, epoch):
         select(models.Campaign)
         .filter_by(title="Test Campaign")).scalar()
 
-    data = {
-        "title": "Valid Epoch",
-        "start_date": "5016/01/05",
-        "end_date": "5016/02/01",
-        "description": "An epoch that should contain "
-                       "the events found in valid_events",
-        "overview": "Overview"
-    }
-    epoch.create(campaign_object, data)
+    epoch_form = CreateEpochForm(
+        MultiDict({
+            "title": "Valid Epoch",
+            "start_year": 5016,
+            "start_month": 1,
+            "start_day": 5,
+            "end_year": 5016,
+            "end_month": 2,
+            "end_day": 1,
+            "overview": "Overview Text",
+        })).data
+    epoch.create(campaign_object, epoch_form)
 
     epoch_object = db.session.execute(
         select(models.Epoch)
         .filter_by(title="Valid Epoch")).scalar()
 
-    # The test epoch starts on 5016/01/05 and ends on 5016/02/01
     for event in epoch_object.events:
         assert epoch_object.has_events is True
         assert event.date.startswith("5016/01") or event.date.startswith("5016/02")
