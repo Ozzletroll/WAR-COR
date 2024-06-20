86f60be0d5ebbfd11d959e260d9f883b1f769808
Harvey <105319430+Ozzletroll@users.noreply.github.com>
2024-06-20 14:11:56 +0100

    Fix horus text styling on campaign page

--- app/static/js/pages/campaign/campaign_page.js
+++ app/static/js/pages/campaign/campaign_page.js
@@ -10,3 +10,9 @@ const toolbar = new Toolbar();
 initialiseCampaignLayout();
 initialiseCampaignMobileTabs();
 initialiseDropdownMenus();
+
+// Style all horus theme text elements
+var horusElements = document.getElementsByClassName("summernote-horus");
+Array.from(horusElements).forEach(element => {
+  element.setAttribute("data-content", element.innerText);
+});
